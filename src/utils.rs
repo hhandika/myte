363cc605cdc1abc95bb201b9b1bc8a3411ba64b5
Heru Handika <herubiolog@gmail.com>
2021-05-22 11:08:56 -0500

    Add test for parsing time.

--- src/utils.rs
+++ src/utils.rs
@@ -12,3 +12,19 @@ pub fn print_formatted_duration(duration: u64) {
     let time = parse_duration(duration);
     println!("Execution time (HH:MM:SS): {}", time);
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn time_parsing_test() {
+        let duration = 65;
+        let duration_2 = 3600;
+        let time = parse_duration(duration);
+        let hours = parse_duration(duration_2);
+
+        assert_eq!("00:01:05", time);
+        assert_eq!("01:00:00", hours);
+    }
+}
