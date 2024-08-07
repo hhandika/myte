5157c3276c14d00eac547a6a95d2cd8e327824e1
Heru Handika <herubiolog@gmail.com>
2024-08-07 17:10:23 -0500

    Fix deprecated code.

--- src/utils.rs
+++ src/utils.rs
@@ -9,7 +9,7 @@ fn parse_duration(duration: u64) -> String {
     let sec = (duration % 60) as u32;
     let min = ((duration / 60) % 60) as u32;
     let hours = ((duration / 60) / 60) as u32;
-    let time = NaiveTime::from_hms(hours, min, sec);
+    let time = NaiveTime::from_hms_opt(hours, min, sec).expect("Invalid time");
     time.format("%H:%M:%S").to_string()
 }
 
